5ea6cf7680d45b4cee6cc5540e195f63629860ed
Fsioutas <fsioutas@hotmail.com>
2023-06-07 10:25:10 +0300

    Add files via upload

--- R/readTechCosts.R
+++ R/readTechCosts.R
@@ -7,11 +7,20 @@ library(quitte)
 # demand / supply
 # sectors: industry, transport, buildings, power&heat, etc.
 # categories specific to each sector
+  if (subtype == "PowerAndHeat") {
+    x <- read.csv("power_and_heat_energyf.csv")
+    names(x) <- c("tech", "value", "year", "variable", "unit")
+    x <- filter(x, x[["tech"]] != "Refurbishment of existing nuclear reactors")
+    x[["value"]] <- as.numeric(x[["value"]])
+    x <- as.quitte(x)
 
-x <- read.csv("power_and_heat_energyf.csv")
-names(x) <- c("tech", "value", "year", "variable", "unit")
-x <- filter(x, x[["tech"]] != "Refurbishment of existing nuclear reactors")
-x[["value"]] <- as.numeric(x[["value"]])
-x <- as.quitte(x)
+  } else if (subtype == "DomesticEnergy"){
+    x <- read.csv("domestic_energyf2.csv")
+    names(x) <- c("tech", "appliances", "sector", "value", "category", "measurement", "variant", "unit")
+    x[["value"]] <- as.numeric(x[["value"]])
+    x <- as.quitte(x)
+  }
+
+
 return(as.magpie(x))
 }
